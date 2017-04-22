fadf201ff388c5a80777b51e2a498f35f55f9493
Jacob Jurilla <iamjake@protonmail.com>
2017-04-22 13:56:17 -0400

    This console class will hold and help print messages

---
+++ console.py
@@ -0,0 +1,15 @@
+class Console(object):
+
+    def log(self, message):
+        print(message)
+
+    def error(self, message):
+        print(message)
+
+    def warn(self, message):
+        print(message)
+
+    def debug(self, message):
+        print(message)
+
+console = Console()
