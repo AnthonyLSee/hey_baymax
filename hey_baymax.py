c3d71bdaf2036b7e45ccc5299a3cb9cf34ef4164
Jacob Jurilla <iamjake@protonmail.com>
2017-04-22 13:35:08 -0400

    initial start

---
+++ hey_baymax.py
@@ -0,0 +1,22 @@
+import tweepy, time, json
+from credentials import *
+import tweetlist as bs
+import catcher as ct
+from console import console
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+console.log("Baymax has started\n")
+
+print "..."
+#for diagnose in ct:
+# def talking():
+#    for line in baymax_quotes.tweetlist:
+#        api.update_status(line)
+#        print(line)
+#        print "..."
+#        time.sleep(40)
+# talking()
