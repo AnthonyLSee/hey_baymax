dedfb75e35b21f5106a8df2786268b3b2e45e145
AnthonyLSee <anthony.see@rutgers.edu>
2017-04-22 19:47:55 -0400

    removal of print
    changes

--- streamclass.py
+++ streamclass.py
@@ -4,4 +4,8 @@ import tweepy
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
+        print("text:" + status.text)
+
+
+    def getID(self,status):
+        print(status.id)
