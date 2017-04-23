2006d08c1083b8cff5b6d8e59dee00f08c33a907
Jacob Jurilla <iamjake@protonmail.com>
2017-04-23 01:30:23 -0400

    updated

--- hey_baymax.py
+++ hey_baymax.py
@@ -1,15 +1,11 @@
 
 import tweepy, time
 import tweetlist as bq
-from catcher import *
+# from catcher import *
 from credentials import *
 from console import console
 from streamclass import *
 
-def respondBack(api,user):     # tweepy Obj and username
-    api.update_status("Hello {}!".format(user))
-
-print(api.get_user(screen_name))
 
 def main():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -24,19 +20,19 @@ def main():
     while(True):
         myStream.filter(track=['@hey_baymax'])
 
-
-
     #
     # mentions = api.mentions_timeline(count=1)
     #
     # for mention in mentions:
     #     print(mention.text)
     #     print(mention.user.screen_name)
-
     # while(True):
     #     pass
     #
     #     # If got message, use respondBack
+
+def respondBack(api,user):
+    api.update_status("Hello {}!".format(user))
 
 if __name__ == "__main__":
     main()
