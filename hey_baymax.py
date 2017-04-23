a11ee96d6e0876cbfcb8fdd24abd924fe8bb5b22
Jacob Jurilla <iamjake@protonmail.com>
2017-04-23 06:28:02 -0400

    cleaner upper

--- hey_baymax.py
+++ hey_baymax.py
@@ -6,7 +6,6 @@ from credentials import *
 from console import console
 from streamclass import *
 
-
 def main():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
@@ -15,21 +14,10 @@ def main():
     myStreamListener = MyStreamListener()
     myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
-    console.log("Baymax has started!\n")
+    console.log("\n\nBaymax Bot is Running...\n")
 
     while(True):
         myStream.filter(track=['@hey_baymax'])
-
-    #
-    # mentions = api.mentions_timeline(count=1)
-    #
-    # for mention in mentions:
-    #     print(mention.text)
-    #     print(mention.user.screen_name)
-    # while(True):
-    #     pass
-    #
-    #     # If got message, use respondBack
 
 def respondBack(api,user):
     api.update_status("Hello {}!".format(user))
