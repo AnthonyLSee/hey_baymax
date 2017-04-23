335d2479d49f13ac1bb07abd893e22d2a3f53405
Jacob Jurilla <iamjake@protonmail.com>
2017-04-22 22:18:33 -0400

    solved screen_name

--- streamclass.py
+++ streamclass.py
@@ -1,10 +1,21 @@
-
 import tweepy
+from credentials import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print("text:" + status.text)
+        userObj = status.user
+        print(userObj.screen_name)
 
+        #print(api.get_user(userObj.screen_name))
 
-    def getID(self,status):
-        print(status.id)
+        #id = status.id
+        #username = api.get_user(id)
+        #print(username)
+
+    #def getID(self,status):
+#        print(status.id)
